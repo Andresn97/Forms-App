38eca25bdaf39074af85f2076c971010c8818ce2
Andrés Novillo <novilloa21@gmail.com>
2023-08-17 11:55:08 -0500

    Register Form: Pending

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import * as customValidators from 'src/app/shared/validators/validators';
+
 
 @Component({
   selector: 'app-register-page',
@@ -7,5 +11,41 @@ import { Component } from '@angular/core';
   ]
 })
 export class RegisterPageComponent {
+
+  public myForm: FormGroup = this._fb.group({
+    name: [
+      '', 
+      [ 
+        Validators.required, 
+        Validators.pattern(customValidators.firstNameAndLastNamePattern)
+      ]
+    ],
+    // email: ['', [ Validators.required, Validators.pattern( this.validatorsService.emailPattern )], [ new EmailValidator() ]],
+    email: [
+      '', 
+      [ 
+        Validators.required, 
+        Validators.pattern(customValidators.emailPattern) 
+      ], 
+      [  ]
+    ],
+    username: ['', [ Validators.required, customValidators.cantBeStrider ]],
+    password: ['', [ Validators.required, Validators.minLength(6) ]],
+    password2: ['', [ Validators.required ]],
+  }, {
+    // validators: [
+    //   this.validatorsService.isFieldOneEqualFieldTwo('password','password2')
+    // ]
+  });
+
+  constructor( private _fb: FormBuilder ) {}
+
+  isValidField( field: string ) {
+    // return this.validatorsService.isValidField( this.myForm, field );
+  }
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+  }
 
 }
